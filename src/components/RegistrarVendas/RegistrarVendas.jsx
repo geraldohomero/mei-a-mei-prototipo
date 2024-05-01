60714736686b6521cb7ac40a0f1f642bb7d7af0a
geraldohomero <geraldohomero@protonmail.com>
2024-04-30 23:11:04 -0300

    update - add GET e POST nos registros e autentica usuário via jwt a partir do ID

--- src/components/RegistrarVendas/RegistrarVendas.jsx
+++ src/components/RegistrarVendas/RegistrarVendas.jsx
@@ -1,68 +1,101 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 
-
 const RegistrarVendas = () => {
-  return (
-    <>
-      <div>
-        <h3>Registre uma venda</h3>
-      </div>
-      <div>
-        <Row className="g-2">
-          <Col md>
-            <FloatingLabel controlId="floatingInputGrid" label="Venda de...">
-              <Form.Control type="venda" placeholder="" />
-            </FloatingLabel>
-          </Col>
-          <Col md>
-            <FloatingLabel
-              controlId="floatingSelectGrid"
-              label="Produto/Serviço"
-            >
-              <Form.Select aria-label="Floating label select example">
-                <option value="1">Produto</option>
-                <option value="2">Serviço</option>
-              </Form.Select>
-            </FloatingLabel>
-          </Col>
-        </Row>
-        <Row className="g-2 mt-1">
-          <Col md>
-            <FloatingLabel controlId="floatingInputGrid" label="Data">
-              <Form.Control type="date" />
-            </FloatingLabel>
-          </Col>
-          <Col md>
-            <FloatingLabel
-              controlId="floatingSelectGrid"
-              label="Meio de pagamento"
-            >
-              <Form.Select aria-label="Floating label select example">
-                <option value="1">Dinheiro</option>
-                <option value="2">Cartão</option>
-                <option value="3">PIX</option>
-                <option value="4">Outro</option>
-              </Form.Select>
-            </FloatingLabel>
-          </Col>
-        </Row>
-        <Row className='g-2 mt-1'>
-          <Col md='auto'>
-            <FloatingLabel controlId="floatingPassword" label="R$">
-              <Form.Control type="monetary" placeholder="Valor" />
-            </FloatingLabel>
-          </Col>
-        </Row>
-        <Button variant="success mt-3">Adicionar venda</Button>{' '}
-      </div>
-    </>
-  )
-}
+  const [venda, setVenda] = useState({
+    id: '',
+    produtosId: [],
+    servicosId: [],
+    nome: '',
+    valor: 0,
+    dataFaturamento: '',
+    meioDePagamento: ''
+  });
 
-export default RegistrarVendas
+  const [produtosServicos, setProdutosServicos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://localhost:5062/api/Produtos')
+      .then(response => response.json())
+      .then(data => setProdutosServicos(data));
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const vendaToSend = {
+      ...venda,
+      produtosId: Array.isArray(venda.produtosId) ? venda.produtosId.map(String) : [],
+      servicosId: Array.isArray(venda.servicosId) ? venda.servicosId.map(String) : [],
+      meioDePagamento: Number(venda.meioDePagamento)
+    };
+
+    fetch('http://localhost:5062/api/Faturamentos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(vendaToSend)
+
+
+    })
+      .then(response => response.json())
+      .then(data => console.log(data));
+  };
+
+  const handleChange = (event) => {
+    setVenda({
+      ...venda,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row className="g-2">
+        <Col md>
+          <FloatingLabel controlId="floatingInputGrid" label="Venda de...">
+            <Form.Control type="text" name="nome" onChange={handleChange} />
+          </FloatingLabel>
+        </Col>
+        <Col md>
+          <FloatingLabel controlId="floatingSelectGrid" label="Produto/Serviço">
+            <Form.Select name="produtosId" onChange={handleChange}>
+              <option value="1">Produto</option>
+              <option value="2">Serviço</option>
+            </Form.Select>
+          </FloatingLabel>
+        </Col>
+      </Row>
+      <Row className="g-2 mt-1">
+        <Col md>
+          <FloatingLabel controlId="floatingInputGrid" label="Data">
+            <Form.Control type="date" name="dataFaturamento" onChange={handleChange} />
+          </FloatingLabel>
+        </Col>
+        <Col md>
+          <FloatingLabel controlId="floatingSelectGrid" label="Meio de pagamento">
+            <Form.Select name="meioDePagamento" onChange={handleChange}>
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão">Cartão</option>
+              <option value="PIX">PIX</option>
+              <option value="Outro">Outro</option>
+            </Form.Select>
+          </FloatingLabel>
+        </Col>
+        <Col>
+          <FloatingLabel controlId="floatingInputGrid" label="Valor">
+            <Form.Control type="number" name="valor" onChange={handleChange} />
+          </FloatingLabel>
+        </Col>
+      </Row>
+      <Button variant="success mt-3" type="submit">Adicionar venda</Button>{' '}
+    </Form>
+  );
+};
+
+export default RegistrarVendas;
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,30 +1,69 @@
-import React from 'react'
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-//import './login.css'
 
 const Login = () => {
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            // Faça a autenticação usando o ID e a senha do usuário
+            const authResponse = await fetch('http://localhost:5062/api/Usuarios/authenticate', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: id, // substitua userId pelo ID do usuário
+                    senha: password
+                })
+            });
+
+            if (!authResponse.ok) {
+                throw new Error('Erro na autenticação');
+            }
+
+            const data = await authResponse.json();
+            const token = data.jwtToken;
+
+            if (!token) {
+                throw new Error('Token não encontrado');
+            }
+
+            // Armazene o token no local storage
+            localStorage.setItem('jwtToken', token);
+
+            // Redirecione o usuário para a página inicial
+            //window.location.href = '/';
+            console.log('Login efetuado com sucesso...jwtToken: ' + token);
+        } catch (error) {
+            console.error('Erro ao fazer login', error);
+        }
+    };
+
     return (
         <>
-            <Form className='form-width m-auto py-5'>
+            <Form className='form-width m-auto py-5' onSubmit={handleSubmit}>
                 <h2>Login</h2>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>E-mail</Form.Label>
-                    <Form.Control type="email" placeholder="nome@email.com" />
-                    
+                <Form.Group className="mb-3">
+                    <Form.Label>Id</Form.Label>
+                    <Form.Control type="id" placeholder="numeros e letras" onChange={e => setId(e.target.value)} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Senha</Form.Label>
-                    <Form.Control type="password" placeholder="Senha" />
+                    <Form.Control type="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)} />
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit" onClick={handleSubmit}>
                     Entrar
                 </Button>
                 <p className="mt-3"> <a className="link-opacity-100" href="/recuperar">Esqueci a senha</a></p>
             </Form>
         </>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
