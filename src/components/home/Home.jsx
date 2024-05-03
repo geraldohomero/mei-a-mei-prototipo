039887c7dd868700470939c40f056e282b529b2d
hugocandian <candian.hugo@gmail.com>
2024-05-02 22:21:31 -0300

    Front-end

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,76 +1,170 @@
 //import React from 'react'
-import Logo from '../../assets/Logo.svg'
-import './home.css'
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import PlayStoreLogo from '../../assets/google-play-badge.png';
-import AppleStoreLogo from '../../assets/apple-store-logo.png';
+import Logo from "../../assets/Logo.svg";
+import dinheiro from "../../assets/dinheiro.svg";
+import grafico from "../../assets/grafico.svg";
+import pcCel from "../../assets/pcCel.svg";
+import whatsapp from "../../assets/whatsapp.svg";
+import email from "../../assets/email.svg";
+import "./home.css";
+import { Container, Row, Col } from "react-bootstrap";
+import PlayStoreLogo from "../../assets/google-play-badge.png";
+import AppleStoreLogo from "../../assets/apple-store-logo.png";
 
 const Home = () => {
   return (
     <>
       <Container>
-        <Row>
-          <Col xs={12} md={6}>
-            <div className='logo'>
-              <img src={Logo} alt="Logo" />
-            </div>
-          </Col>
-          <Col xs={12} md={6} className='align-self-center text-center'>
-            <h1>Organize seu negócio!</h1>
-            <h3>Registre e gerencie tudo com mais facilidade.</h3>
-            <div className="mb-2 mt-4">
-              <Button href="/login" className="btn-entrar" size="lg">
-                Entrar
-              </Button>{' '}
-              <Button href="/registrar" className="btn-cadastrar" size="lg">
-                Cadastrar
-              </Button>
-            </div>
-          </Col>
-        </Row>
-        <Row className="mt-5" >
-          <Col xs={12} md={3} className="stores text-center">
-            <a href="https://play.google.com/store/apps/details?id=com.example.app" target="_blank" rel="noopener noreferrer">
-              <img src={PlayStoreLogo} alt="Download na Play Store" className="store-logo" />
+        <section>
+          <Row>
+            <Col className="m-auto align-items-center">
+              <Row>
+                <Col className="col-2"></Col>
+                <Col className="col-6">
+                  <h1 className="mb-4">
+                    <span className="purple-text fw-bold">
+                      Organize seu negócio!
+                    </span>
+                  </h1>
+                  <div className="fw-bol mb-4">
+                    <h3>
+                      <span className="purple-text">Registre</span> e{" "}
+                      <span className="purple-text">gerencie</span> tudo{" "}
+                      <span className="purple-text">com</span> mais{" "}
+                      <span className="purple-text">facilidade!</span>
+                    </h3>
+                  </div>
 
-            </a>
-            <a href="https://apps.apple.com/app/id123456789" target="_blank" rel="noopener noreferrer" >
-              <img src={AppleStoreLogo} alt="Download na Apple Store" className="store-logo" />
-
-            </a>
-          </Col>
-          <Col xs={12} md={6} className='align-self-center text-center sobre'>
-            <h2>Sobre o MEI a MEI</h2>
-            <p>É um aplicativo e website permitem aos empresários que usam MEI acompanhar suas finanças para que não ultrapassem o
-              limite definido por lei para a categoria. Com ele, você pode registrar e gerenciar todas as suas transações financeiras,
-              tornando mais fácil manter-se dentro do limite de receita bruta anual.</p>
-            <p>Você também pode usar o aplicativo pelo website ou baixá-lo para seu dispositivo móvel</p>
-          </Col>
-        </Row>
-        <Row className="mt-5 mb-5" >
-          <Col xs={20} className="text-center mb-5">
-            <h1>Funcionalidades</h1>
-          </Col>
-          <Col xs={12} md={4} className="mt-3 text-center">
-            <h3>Gerenciamento Financeiro</h3>
-            <p>Registre e gerencie todas as suas transações financeiras de forma fácil e intuitiva.</p>
-            <img src="https://via.placeholder.com/150x300" alt="Imagem placeholder" />
-          </Col>
-          <Col xs={12} md={4} className="mt-3 text-center">
-            <h3>Acompanhamento de Limites</h3>
-            <p>Acompanhe suas finanças para garantir que você não ultrapasse o limite definido por lei para a categoria MEI.</p>
-            <img src="https://via.placeholder.com/150x300" alt="Imagem placeholder" />
-          </Col>
-          <Col xs={12} md={4} className="mt-3 text-center">
-            <h3>Multiplataforma</h3>
-            <p>Use o aplicativo pelo website ou baixá-lo para seu dispositivo móvel.</p>
-            <img src="https://via.placeholder.com/150x300" alt="Imagem placeholder" />
-          </Col>
-        </Row>
-
-      </Container >
+                  <p className="fw-medium">
+                    Sistema Financeiro simples e seguro. Automatize as rotinas
+                    financeiras do seu MEI e tenha suas informações organizadas.
+                  </p>
+                </Col>
+              </Row>
+            </Col>
+            <Col className="align-self-center text-center">
+              <div className="logo img-fluid">
+                <img src={Logo} alt="Logo" />
+              </div>
+            </Col>
+          </Row>
+        </section>
+        <section className="funcionalidades-bg purple-text">
+          <Row>
+            <h3 className="purple-text text-center mt-3" id="funcionalidades">
+              Funcionalidades
+            </h3>
+          </Row>
+          <Row className="mt-5 mx-2">
+            <Col className="col-4 d-flex gap-2">
+              <Col className="col-4">
+                <div className="d-flex justify-content-end mt-1 me-4">
+                  <img className="dinheiro" src={dinheiro} alt="" />
+                </div>
+              </Col>
+              <Col>
+                <h5 className="mb-3">Gerenciamento Financeiro</h5>
+                <p className="text-dark fw-medium">
+                  Registre e gerencie todas as suas transações financeiras de
+                  forma fácil e intuitiva.
+                </p>
+              </Col>
+            </Col>
+            <Col className="col-4 d-flex  gap-2">
+              <Col className="col-4">
+                <div className="d-flex justify-content-end mt-1 me-4">
+                  <img className="grafico" src={grafico} alt="" />
+                </div>
+              </Col>
+              <Col>
+                <h5 className="mb-3">Acompanhamento de Limites</h5>
+                <p className="text-dark fw-medium">
+                  Acompanhe suas finanças para garantir que você não ultrapasse
+                  o limite definido por lei para a categoria MEI.
+                </p>
+              </Col>
+            </Col>
+            <Col className="col-4 d-flex  gap-2">
+              <Col className="col-4">
+                <div className="d-flex justify-content-end mt-1 me-4">
+                  <img className="pcCel" src={pcCel} alt="" />
+                </div>
+              </Col>
+              <Col>
+                <h5 className="mb-3">Multiplataforma</h5>
+                <p className="text-dark fw-medium">
+                  Use o aplicativo pelo website ou baixá-lo para seu dispositivo
+                  móvel.
+                </p>
+              </Col>
+            </Col>
+          </Row>
+        </section>
+        <section>
+          <Row>
+            <Col className="d-flex">
+              <Col className="col-2"></Col>
+              <Col className="col-6">
+                <div className="align-items-center justify-items-center d-block">
+                  <h5
+                    className="purple-text text-start mb-3 mt-5"
+                    id="faleconosco"
+                  >
+                    Fale Conosco:
+                  </h5>
+                  <div className="d-flex align-items-center fw-medium purple-text">
+                    <img className="whatsapp my-3" src={whatsapp} alt="" />
+                    <p className="m-0 ms-3">55 31 99266-7723</p>
+                  </div>
+                  <div className="d-flex align-items-center fw-medium purple-text">
+                    <img className="email my-3" src={email} alt="" />
+                    <p className="m-0 ms-3">meiamei@contato.com</p>
+                  </div>
+                </div>
+              </Col>
+            </Col>
+            <Col className="d-flex">
+              <Col className="col-2"></Col>
+              <Col className="col-6">
+                <div className="align-items-center justify-items-center d-block">
+                  <h5
+                    className="purple-text text-start mb-3 mt-5"
+                    id="faleconosco"
+                  >
+                    Baixe nosso aplicativo:
+                  </h5>
+                  <div className="align-items-start">
+                    <a
+                      href="https://play.google.com/store/apps/details?id=com.example.app"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        src={PlayStoreLogo}
+                        alt="Download na Play Store"
+                        className="store-logo"
+                      />
+                    </a>
+                    <a
+                      href="https://apps.apple.com/app/id123456789"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        src={AppleStoreLogo}
+                        alt="Download na Apple Store"
+                        className="store-logo "
+                      />
+                    </a>
+                  </div>
+                </div>
+              </Col>
+            </Col>
+          </Row>
+        </section>
+      </Container>
+      <footer className="footer"></footer>
     </>
-  )
-}
+  );
+};
 
 export default Home;
--- src/components/registrar/Registrar.jsx
+++ src/components/registrar/Registrar.jsx
@@ -1,24 +1,26 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import axios from 'axios';
-import './registrar.css';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import axios from "axios";
+import { Row, Col, Container } from "react-bootstrap";
+import Logo from "../../assets/Logo.svg";
+import "./registrar.css";
 
 function Registrar() {
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [nome, setNome] = useState('');
-  const [cnpj, setCnpj] = useState('');
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [nome, setNome] = useState("");
+  const [cnpj, setCnpj] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:5062/api/Usuarios', {
+      const response = await axios.post("http://localhost:5062/api/Usuarios", {
         email,
         senha,
         nome,
-        cnpj
+        cnpj,
       });
 
       console.log(response.data);
@@ -26,45 +28,79 @@ function Registrar() {
       const token = response.data.token;
 
       // Armazene o token no local storage
-      localStorage.setItem('jwtToken', token);
-      console.log('Token JWT via E-mail: ' + token);
+      localStorage.setItem("jwtToken", token);
+      console.log("Token JWT via E-mail: " + token);
 
       // Redirecione o usuário para a página inicial
       // window.location.href = '/';
     } catch (error) {
-      console.error('Erro ao registrar', error);
+      console.error("Erro ao registrar", error);
     }
   };
 
   return (
-    <Form className='form-width m-auto py-5'>
-      <h2>Faça seu cadastro!</h2>
+    <div>
+      <Container className="view">
+        <div className="box">
+          <Row className="mt-3">
+            <Col className="col-7 bg-left">
+              <Row>
+                <div className="text-center">
+                  <h1>Criar Conta</h1>
+                </div>
+              </Row>
+              <Row>
+                <Form className="form-width m-auto py-5">
+                  <h2>Faça seu cadastro!</h2>
 
-      <Form.Group className="mb-3" controlId="formBasicNome">
-        <Form.Label>Nome</Form.Label>
-        <Form.Control type="name" placeholder="Seu nome" onChange={e => setNome(e.target.value)} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>E-mail</Form.Label>
-        <Form.Control type="email" placeholder="nome@email.com" onChange={e => setEmail(e.target.value)} />
-        <Form.Text className="text-muted">
-          Nós nunca compartilharemos seu e-mail.
-        </Form.Text>
-      </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>E-mail</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="nome@email.com"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Form.Text className="text-muted">
+                      Nós nunca compartilharemos seu e-mail.
+                    </Form.Text>
+                  </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Senha</Form.Label>
-        <Form.Control type="password" placeholder="Senha" onChange={e => setSenha(e.target.value)} />
-      </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Senha</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Senha"
+                      onChange={(e) => setSenha(e.target.value)}
+                    />
+                  </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicCnpj">
-        <Form.Label>CNPJ</Form.Label>
-        <Form.Control type="text" placeholder="CNPJ" onChange={e => setCnpj(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Enviar
-      </Button>
-    </Form>
+                  <Form.Group className="mb-3" controlId="formBasicCnpj">
+                    <Form.Label>CNPJ</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="CNPJ"
+                      onChange={(e) => setCnpj(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    onClick={handleSubmit}
+                  >
+                    Enviar
+                  </Button>
+                </Form>
+              </Row>
+            </Col>
+            <Col className="col-5 d-flex justify-content-center align-self-center">
+              <div className="">
+                <img className="logo" src={Logo} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </div>
   );
 }
 
